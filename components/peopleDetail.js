5d3dfdc99656dc5b56aed654ceb6cb4be3b45e94
Jacob <83643284+jungjun-Sung@users.noreply.github.com>
2021-05-25 16:29:55 +0900

    return 안넣은거 추가함

--- components/peopleDetail.js
+++ components/peopleDetail.js
@@ -4,16 +4,16 @@ import { connect } from 'react-redux';
 class PeopleDetail extends Component {
   render() {
     if (!this.props.person) {
-      <div>sorry</div>;
+      return <div>sorry</div>;
     }
 
     return (
       <div>
-        <h3>This is {this.props.person.name}'s information</h3>
-        <div>{this.props.person.name}</div>
-        <div>{this.props.person.age}</div>
+        <h3>This is {this.props.person.name}'s imformation</h3>
+        <div>Name : {this.props.person.name}</div>
+        <div>Age : {this.props.person.age}</div>
       </div>
-    );ㄴ
+    );
   }
 }
 
